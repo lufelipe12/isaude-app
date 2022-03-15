32819a1d8a944e3a525bf13458ffa7cadff8ad19
Larissa Paulino <larissaspaulino@gmail.com>
2022-03-15 20:27:39 -0300

    style: pallette color on global style

--- src/styles/global.js
+++ src/styles/global.js
@@ -9,23 +9,26 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     :root {
-      --white: #F3FFE2;
+      --white: #FFFFFF;
+      --beige: #F3FFE2;
       --dark-blue: #021E33;
       --blue-1: #225378;
       --blue-2: #1695A3;
-      --light-blue: #ACF0F2;
+      --light-blue-1: #ACF0F2;
+      --light-blue-2:  #EEFFFF;
       --orange: #EB7F00;
       --green: #11A131;
       --black: #000;
+      --error: #ff0000;
+      --success: #3FE864;
     }
 
     body {
-        background-color: var(--vanilla);
-        color: var(--black);
+  
     }
 
     body, input, button {
-        font-family: 'Roboto', sans-serif;
+        font-family: 'Inter', sans-serif;
         font-size: 1rem;
     }
 
