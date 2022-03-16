6cd72a6392794d0961694425af465b1a028d786e
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-03-16 10:19:39 -0300

    Feature: add assets file

---
+++ src/components/Header/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  width: 100%;
+  height: 150px;
+  padding: 0px 20px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: var(--dark-blue);
+
+  img {
+    width: 120px;
+    height: 90px;
+  }
+`;
