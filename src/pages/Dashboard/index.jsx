72bce8a61ef37c098e701c2e9cc7e7f792f3f03e
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-03-16 19:43:41 -0300

    Fix: fix conflicts

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,49 +1,51 @@
-import { useVaccines } from '../../providers/vaccines'
-import Card from '../../components/Card'
+import { useVaccines } from "../../providers/vaccines";
+import Card from "../../components/Card";
 
 const Dashboard = () => {
   // const { vaccines } = useVaccines()
   const vaccines = [
     {
       userId: 2,
-      name: 'Covid-19',
-      manufacturer: 'Oxford',
+      name: "Covid-19",
+      manufacturer: "Oxford",
       lote: 2337593279,
-      applicationDate: '19/01/2022',
+      applicationDate: "19/01/2022",
       shot: 2,
-      location: 'Araguari',
+      location: "Araguari",
       totalShots: 2,
-      nextShot: '20/04/2022',
+      nextShot: "20/04/2022",
     },
     {
       userId: 2,
-      name: 'Covid-19',
-      manufacturer: 'Oxford',
+      name: "Covid-19",
+      manufacturer: "Oxford",
       lote: 2337593279,
-      applicationDate: '19/01/2022',
+      applicationDate: "19/01/2022",
       shot: 2,
-      location: 'Araguari',
+      location: "Araguari",
       totalShots: 2,
-      nextShot: '20/04/2022',
+      nextShot: "20/04/2022",
     },
     {
       userId: 2,
-      name: 'Covid-19',
-      manufacturer: 'Oxford',
+      name: "Covid-19",
+      manufacturer: "Oxford",
       lote: 2337593279,
-      applicationDate: '19/01/2022',
+      applicationDate: "19/01/2022",
       shot: 2,
-      location: 'Araguari',
+      location: "Araguari",
       totalShots: 2,
-      nextShot: '20/04/2022',
+      nextShot: "20/04/2022",
     },
-  ]
+  ];
 
-  return <div>
-    {vaccines.map((vaccine, index) => (
-      <Card vaccine={vaccine} key={index}/>
-    ))}
-  </div>
-}
+  return (
+    <div>
+      {vaccines.map((vaccine, index) => (
+        <Card vaccine={vaccine} key={index} />
+      ))}
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
