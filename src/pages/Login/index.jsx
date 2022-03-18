31b6804e6e7c329663f8a261341d5bd6c7af7e6c
Larissa Paulino <larissaspaulino@gmail.com>
2022-03-18 10:43:09 -0300

    fix: login is totally functional now

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,12 +1,13 @@
-import Input from "../../components/Input";
-import Button from "../../components/Button";
+import Input from "../../components/Input"
+import Button from "../../components/Button"
 
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import { useHistory } from 'react-router-dom'
 
-import { useUser } from "../../providers/user";
-import api from "../../services/api";
+import { useUser } from "../../providers/user"
+import api from "../../services/api"
 
 import {
   Container,
@@ -15,20 +16,25 @@ import {
   Form,
   Label,
   Logo,
-} from "./styles";
-import logo from "../../assets/logo.png";
+} from "./styles"
+import logo from "../../assets/logo.png"
 
 const Login = () => {
+
+  const history = useHistory()
+  
+  const { login } = useUser()
+  
   const loginSchema = yup.object().shape({
     email: yup.string().email("Email inválido").required("Campo Obrigatório"),
     password: yup
       .string()
-      .matches(
-        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/,
-        "Senha fraca"
-      )
-      .required("Campo Obrigatório"),
-  });
+      // .matches(
+      //   /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/,
+      //   "Senha fraca"
+      // )
+      .required("Campo Obrigatório")
+  })
 
   const {
     register,
@@ -36,19 +42,10 @@ const Login = () => {
     formState: { errors },
   } = useForm({
     resolver: yupResolver(loginSchema),
-  });
+  })
 
-  // const {login} = useUser();
 
-  const handleLogin = (data) => {
-    // login(data);
-    // api.post("/login", data)
-    // .then((response)=> {
-    //   const {accessToken, user} = response.data;
-    //   localStorage.setItem("@Isaúde:token", JSON.stringify(accessToken));
-    //   localStorage.setItem("@Isaúde:user", JSON.stringify(user));
-    // }).catch((err) => console.log("Email ou senha invalidos"))
-  };
+  const handleLogin = (data) => login(data)
 
   return (
     <Container>
@@ -58,35 +55,35 @@ const Login = () => {
       <ContainerForm>
         <Form onSubmit={handleSubmit(handleLogin)}>
           <h1>Login</h1>
-          <Label>E-mail</Label>
+          <Label></Label>
           <Input
-            name="email" // o que vai ser passado para o register
-            label="Digite seu E-mail" // placeholder
+            name="email" 
+            label="Email" 
             type="email"
             register={register}
             helperText={errors.email?.message}
             error={!!errors.email?.message}
           />
-          <Label>Senha</Label>
+          <Label></Label>
           <Input
-            name="password" // o que vai ser passado para o register
-            label="Digite sua Senha" // placeholder
+            name="password" 
+            label="Senha" 
             type="password"
             register={register}
             helperText={errors.password?.message}
             error={!!errors.password}
           />
           <ContainerButton>
-            <Button colorType="Primary">Entrar</Button>
+            <Button colorType="primary" type='submit'>Entrar</Button>
           </ContainerButton>
 
           <span>Ainda não possui uma conta?</span>
           <ContainerButton>
-            <Button colorType="Tertiary">Cadastre-se</Button>
+            <Button colorType="terciary" onClick={() => history.push("/register")}>Cadastre-se</Button>
           </ContainerButton>
         </Form>
       </ContainerForm>
     </Container>
-  );
-};
-export default Login;
+  )
+}
+export default Login
