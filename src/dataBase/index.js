37ed0abc0e07078dc09e5a29d519e7404323bd57
NehamaMandelbaum <nehama1836@gmail.com>
2022-03-21 13:08:58 -0300

    feature: curiosidades component done

---
+++ src/dataBase/index.js
@@ -0,0 +1,8 @@
+export const historyParagraph =
+  " A história das vacinas é muito mais antiga do que imaginam! No século XVIII o médico inglês Edward Jenner utilizou a vacina para prevenir acontaminação por varíola, uma doença viral extremamente grave quecausava febre alta, dores de cabeça e no corpo, lesões na pele e morte. "
+
+export const importanceParagraph =
+  " A vacinação é uma forma de salvar vidas! Uma vez expostos a uma, ou mais doses de uma vacina, normalmente permanecemos protegidos contra uma doença por anos, décadas ou mesmo por todaa vida. É isso que torna as vacinas tão eficazes. Ao invés de tratar uma doença depois que ela ocorre, as vacinas nos previnem de adoecer."
+
+export const erradicationParagraph =
+  "A exemplo da importância das vacinas já temos doenças que foram erradicadas ou muito bem controlada por elas, como:  1 - Varíola, completamente erradicada mundialmente. 2 - Poliomielite, erradicada no território nacional mas com altas chances de voltar devido à baixa adesão a vacinação. 3 - Sarampo chegou a ser erradicada mas devido à falta de vacinação voltou a aparecer."
