f2ff5f593ff131f7751f31320312d104480510f4
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-03-21 19:51:33 -0300

    Fix: conflicts

--- src/components/NewsCarousel/index.jsx
+++ src/components/NewsCarousel/index.jsx
@@ -1,51 +1,51 @@
-import Carousel from 'react-material-ui-carousel'
-import { CarouselCard } from '../CarouselCard'
-import { ContainerCarousel } from './styles'
-import covid from '../../assets/covid.jpeg'
-import vaccine from '../../assets/vacinação.jpeg'
-import tranmition from '../../assets/transmissão.jpeg'
-import { useState } from 'react'
+import Carousel from "react-material-ui-carousel";
+import { CarouselCard } from "../CarouselCard";
+import { ContainerCarousel } from "./styles";
+import covid from "../../assets/covid.jpeg";
+import vaccine from "../../assets/vacinação.jpeg";
+import tranmition from "../../assets/transmissão.jpeg";
+import { useState } from "react";
 
 export const NewsCarousel = () => {
   // Lista de itens, que sera substituida por uma lista de notícias.
   let news = [
     {
-      title: 'Nova variante',
-      date: '11/03/2022',
+      title: "Nova variante",
+      date: "11/03/2022",
       img: covid,
     },
     {
-      title: 'Uso de máscaras',
-      date: '10/03/2022',
+      title: "Uso de máscaras",
+      date: "10/03/2022",
       img: vaccine,
     },
     {
-      title: 'Segunda dose',
-      date: '17/03/2022',
+      title: "Segunda dose",
+      date: "17/03/2022",
       img: tranmition,
     },
     {
-      title: 'Uso de máscaras',
-      date: '10/03/2022',
+      title: "Uso de máscaras",
+      date: "10/03/2022",
       img: vaccine,
     },
-  ]
+  ];
 
-  let newArr = []
+  let newArr = [];
   const newsPortions = () => {
     for (let i = 0; i <= Math.floor(news.length / 3); i++) {
-      newArr.push([])
+      newArr.push([]);
     }
-    news.forEach((item, index) => newArr[Math.floor(index / 3)]?.push(item))
-    return newArr
-  }
-  newsPortions()
+    news.forEach((item, index) => newArr[Math.floor(index / 3)]?.push(item));
+    return newArr;
+  };
+  newsPortions();
 
   return (
     <ContainerCarousel>
       {/* CointainerCarousel é apenas um componente de estilização */}
       <Carousel
-        className='Carousel'
+        className="Carousel"
         // Coursel é o componente da biblioteca, next e prev fazem a troca automática
         next={(next, active) =>
           console.log(`we left ${active}, and are now at ${next}`)
@@ -54,20 +54,20 @@ export const NewsCarousel = () => {
           console.log(`we left ${active}, and are now at ${prev}`)
         }
       >
-        {window.matchMedia('(min-width:600px)').matches
+        {window.matchMedia("(min-width:600px)").matches
           ? newArr.map((item) => (
-              <div className='CarouselBox'>
+              <div className="CarouselBox">
                 {item.map((card, i) => (
                   <CarouselCard key={i} item={card} />
                 ))}
               </div>
             ))
           : news.map((item, i) => (
-              <div className='CarouselBox'>
+              <div className="CarouselBox">
                 <CarouselCard key={i} item={item} />
               </div>
             ))}
       </Carousel>
     </ContainerCarousel>
-  )
-}
+  );
+};
