caaa8cb11bbf31b9815782bfe9c55f0d43f3f51b
Murilo <mu.violao@gmail.com>
2022-03-23 10:33:43 -0300

    fix: input register and icon-home in register and login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,18 +1,18 @@
-import Input from '../../components/Input'
-import Button from '../../components/Button'
-import { useUser } from '../../providers/user'
-import MotionRoutes from '../../motionRoutes'
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import { useUser } from "../../providers/user";
+import MotionRoutes from "../../motionRoutes";
 
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as yup from 'yup'
-import { useHistory } from 'react-router-dom'
-import InputAdornment from '@mui/material/InputAdornment'
-import Visibility from '@mui/icons-material/Visibility'
-import VisibilityOff from '@mui/icons-material/VisibilityOff'
-import IconButton from '@mui/material/IconButton'
-
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useHistory } from "react-router-dom";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import IconButton from "@mui/material/IconButton";
+import { AiOutlineHome } from "react-icons/ai";
 
 import {
   Container,
@@ -21,22 +21,25 @@ import {
   Form,
   Label,
   Logo,
-} from './styles'
-import logo from '../../assets/logo.png'
+  ImageHome,
+} from "./styles";
+import logo from "../../assets/logo.png";
 
 const Login = () => {
-  const history = useHistory()
+  const history = useHistory();
 
-  const { login } = useUser()
+  const { login } = useUser();
 
-  const [showPassword, setShowPassword] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
+
+  const backHome = () => {
+    history.push("/");
+  };
 
   const loginSchema = yup.object().shape({
-    email: yup.string().email('Email inválido').required('Campo Obrigatório'),
-    password: yup
-      .string()
-      .required('Campo Obrigatório'),
-  })
+    email: yup.string().email("Email inválido").required("Campo Obrigatório"),
+    password: yup.string().required("Campo Obrigatório"),
+  });
 
   const {
     register,
@@ -44,58 +47,61 @@ const Login = () => {
     formState: { errors },
   } = useForm({
     resolver: yupResolver(loginSchema),
-  })
+  });
 
-  const handleLogin = (data) => login(data)
+  const handleLogin = (data) => login(data);
 
-  const handleClickShowPassword = () => setShowPassword(!showPassword)
-  const handleMouseDownPassword = (event) => event.preventDefault()
-  
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const handleMouseDownPassword = (event) => event.preventDefault();
+
   const inputPropsPassword = {
     endAdornment: (
-      <InputAdornment position='end'>
+      <InputAdornment position="end">
         <IconButton
-          aria-label='toggle password visibility'
+          aria-label="toggle password visibility"
           onClick={handleClickShowPassword}
           onMouseDown={handleMouseDownPassword}
-          edge='end'
+          edge="end"
         >
           {showPassword ? <VisibilityOff /> : <Visibility />}
         </IconButton>
       </InputAdornment>
     ),
-  }
+  };
 
   return (
     <MotionRoutes>
       <Container>
+        <ImageHome onClick={backHome}>
+          <AiOutlineHome />
+        </ImageHome>
         <Logo>
-          <img src={logo} alt='I saúde' />
+          <img src={logo} alt="I saúde" />
         </Logo>
         <ContainerForm>
           <Form onSubmit={handleSubmit(handleLogin)}>
             <h1>Login</h1>
             <Label></Label>
             <Input
-              name='email'
-              label='Email'
-              type='email'
+              name="email"
+              label="Email"
+              type="email"
               register={register}
               helperText={errors.email?.message}
               error={!!errors.email?.message}
             />
             <Label></Label>
             <Input
-              name='password'
-              label='Senha'
-              type={showPassword ? 'text' : 'password'}
+              name="password"
+              label="Senha"
+              type={showPassword ? "text" : "password"}
               register={register}
               helperText={errors.password?.message}
               error={!!errors.password}
               InputProps={inputPropsPassword}
             />
             <ContainerButton>
-              <Button colorType='primary' type='submit'>
+              <Button colorType="primary" type="submit">
                 Entrar
               </Button>
             </ContainerButton>
@@ -103,8 +109,8 @@ const Login = () => {
             <span>Ainda não possui uma conta?</span>
             <ContainerButton>
               <Button
-                colorType='terciary'
-                onClick={() => history.push('/register')}
+                colorType="terciary"
+                onClick={() => history.push("/register")}
               >
                 Cadastre-se
               </Button>
@@ -113,6 +119,6 @@ const Login = () => {
         </ContainerForm>
       </Container>
     </MotionRoutes>
-  )
-}
-export default Login
+  );
+};
+export default Login;
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,4 +1,4 @@
-import styled, {keyframes} from "styled-components"
+import styled, { keyframes } from "styled-components";
 
 const appearFromLeft = keyframes`
     from {
@@ -10,15 +10,18 @@ const appearFromLeft = keyframes`
         opacity: 1;
         transform: translateX(0px)
     }
-`
+`;
 
 export const Container = styled.div`
-  width: 300px;
+  width: 83%;
   max-width: 370px;
-  height: 680px;
+  min-height: 700px;
+  height: 100%;
   background: var(--white);
   border-radius: 10px;
   animation: ${appearFromLeft} 1.4s;
+  margin-top: 50px;
+  margin-bottom: 50px;
 
   form {
     display: flex;
@@ -26,8 +29,15 @@ export const Container = styled.div`
     justify-content: space-between;
     margin: 0 auto;
     width: 100%;
-    height: 680px;
+    height: 100%;
     padding: 20px;
+
+    .MuiTextField-root {
+      margin-bottom: 10px;
+    }
+    .MuiFormControl-root {
+      margin-bottom: 10px;
+    }
   }
 
   span {
@@ -61,29 +71,34 @@ export const Container = styled.div`
 
   @media (min-width: 768px) {
     width: 40%;
-    height: 680px;
+    height: 100%;
     margin-left: 50px;
     form {
       width: 100%;
-      height: 680px;
+      height: 100%;
     }
   }
-`
+`;
 
 export const ContainerRow = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-`
+`;
 
 export const FullContainer = styled.div`
   display: flex;
+  flex-direction: column-reverse;
   justify-content: center;
   align-items: center;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background: #021e33;
-`
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
 export const ImageContainer = styled.div`
   width: 60%;
   height: 100vh;
@@ -98,4 +113,16 @@ export const ImageContainer = styled.div`
     display: flex;
     justify-content: center;
   }
-`
+`;
+
+export const ImageHome = styled.span`
+  align-self: flex-start;
+  background: var(--light-blue-2);
+  border-radius: 100px;
+  padding: 5px;
+  margin-top: 15px;
+  margin-left: 15px;
+  @media (min-width: 768px) {
+    margin-right: 15px;
+  }
+`;
